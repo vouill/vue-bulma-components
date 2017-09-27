8496c776008aef0acf28d533ba251fd0a6eb0115
Vouill <vouillon.thibaud@gmail.com>
2017-09-27 17:48:58 +0200

    Fixed text

--- src/example/more.vue
+++ src/example/more.vue
@@ -2,8 +2,8 @@
   <div class="vbc-buttons">
     <b-content>
       <h2>Bulma styles.</h2>
-      <p>This library simply handle the class syntax as a component & directive syntax.</p>
-      <p>This means that you have full access to all bulma style, you can also use it with custom bulma css :).</p>
+      <p>This library simply handle the class syntax as a component & directive syntax, it' up to you to choose how you want to install Bulma.</p>
+      <p>This means that you have full access to all bulma style, you can also use it with your custom bulma css :).</p>
       <h2>Try it live!</h2>
       <a href="https://codesandbox.io/s/wk2w3z0zk5">Click here</a>
     </b-content>
